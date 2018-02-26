79b8d9d5d01db17b8d35b45e571106f8732912d4
Ryan Dabler <rpdabler@gmail.com>
2018-02-26 13:10:49 -0700

    run full test suite

--- test/test-parser.js
+++ test/test-parser.js
@@ -16,52 +16,52 @@ describe("Parser", function() {
     describe("_parseHeaders", function() {
         it("Should parse well-formed headers", function() {
             const wikitext = `=A=
-    ==B==
-    ===C===
-    ==D==
-    ===E===
-    ====F====
-    =====G=====
-    ====H====
-    =====I=====
-    ======J======`;
+==B==
+===C===
+==D==
+===E===
+====F====
+=====G=====
+====H====
+=====I=====
+======J======`;
             const result = parser._parseHeaders(wikitext);
             const answer = `<h1>A</h1>
-    <h2>B</h2>
-    <h3>C</h3>
-    <h2>D</h2>
-    <h3>E</h3>
-    <h4>F</h4>
-    <h5>G</h5>
-    <h4>H</h4>
-    <h5>I</h5>
-    <h6>J</h6>`;
+<h2>B</h2>
+<h3>C</h3>
+<h2>D</h2>
+<h3>E</h3>
+<h4>F</h4>
+<h5>G</h5>
+<h4>H</h4>
+<h5>I</h5>
+<h6>J</h6>`;
             expect(result).to.equal(answer);
         });
 
         it("Should not parse malformed headers", function() {
             const wikitext = `=A=a
-    b==B==`;
+b==B==`;
             const result = parser._parseHeaders(wikitext);
             const answer = `=A=a
-    b==B==`;
+b==B==`;
             expect(result).to.equal(answer);
         });
 
         it("Should correctly render unexpected edge cases", function() {
             const wikitext = `===
-    ==a===
-    === =`;
+==a===
+=== =`;
             const result = parser._parseHeaders(wikitext);
             const answer = `<h1>=</h1>
-    <h2>a=</h2>
-    <h1>== </h1>`;
+<h2>a=</h2>
+<h1>== </h1>`;
             expect(result).to.equal(answer);
         });
     });
 
     describe("_parseLinks", function() {
-        it.only("Should parse links without piping", function() {
+        it("Should parse links without piping", function() {
             const wikitext = ["[[abc]]", "[[pip_ing]]", "[[aba$ip]]", "[[aaa()bbb]]", "[[a b]]"];
             const parseFn  = parser._parseLinks.bind(parser);
             const result   = wikitext.map(parseFn);
