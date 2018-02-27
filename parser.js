428bc1647aae4044aaef97ba16aa6c267882d277
Ryan Dabler <rpdabler@gmail.com>
2018-02-27 15:22:50 -0700

    Implement full parsing of internal wiki links

--- parser.js
+++ parser.js
@@ -16,6 +16,40 @@ class Parser {
         return `${protocol}://${subdomain}.${domain}.${tld}/${path}`;
     }
 
+    isValidSisterWiki(designation) {
+        const found = this.settings.interwiki.filter(item => {
+            return item.indicators.find(indicator => indicator === designation) ? true : false;
+        });
+        return found ? found[0] : false;
+    }
+
+    isValidSubdomain(designation) {
+        const wiki = this.settings.interwiki.find(item => 
+            item.server.domain === this.settings.server.domain
+        );
+        const subdomain = wiki && wiki.subdomains
+                                ? wiki.subdomains.find(subdomain => subdomain === designation)
+                                : null;
+        return subdomain ? true : false;
+    }
+
+    _pipeLink(newP6, p5, p6) {
+        if (p5 && p6 !== "") {
+            return p6;
+        }
+
+        if (p5 && p6 === "") {
+            const segments = newP6.split(":");
+            if (segments.length > 1) {
+                segments.shift();
+            }
+
+            return segments.join(":");
+        }
+
+        return newP6;
+    }
+    
     _parseHeaders(wikimarkup) {
         let text = wikimarkup;
         for (let n = 6; n > 0; n--) {
@@ -27,12 +61,51 @@ class Parser {
     
     _parseLinks(wikimarkup) {
         const createLink = (function cl(match, p1, p2, p3, p4, p5, p6, offset, string) {
-            // Process the href portion of the link
-            let newP4 = this.settings.allowLowerCase ? p4
-                                                     : p4.charAt(0).toUpperCase() + p4.slice(1);
-            newP4 = newP4.replace(" ", "_");
+            const server = JSON.parse(JSON.stringify(this.settings.server));
+            let newP4 = p4,
+                newP6 = p6;
 
-            return `<a href='${this.getBaseURL() + "/" + newP4}'>${p6 ? p6 : p4}</a>`;
+            if (p1) {
+                // We have a link with a colon - could be interwiki, inter-subdomain, or
+                // internamespace
+                const segments = p1.split(":");
+                const firstSeg = segments[0];
+                const sister   = this.isValidSisterWiki(firstSeg);
+
+                newP6 = `${p1}${p4}`;
+                if (sister) {
+                    const { domain, tld } = sister.server;
+                    Object.assign(server, { domain, tld });
+                    newP4 = p3 ? `${p3}:${p4}` : newP4;
+                } else if (this.isValidSubdomain(firstSeg)) {
+                    Object.assign(server, { subdomain: firstSeg });
+                    
+                    // Subdomain links don't display unless they are preceded by a ":"
+                    if (match.charAt(2) !== ":") return "";
+                } else if (firstSeg === this.settings.categoryNS) {
+                    // We are linking to a category. If first character of match is a ":"
+                    // we will display it, otherwise we will return an empty string
+                    if (match.charAt(2) === ":") {
+                        newP4 = `${p1}${p4}`;
+                    } else {
+                        return "";
+                    }
+                } else {
+                    newP6 = `${p1}${p4}`;
+                    newP4 = `${p1}${p4}`;
+                    newP4 = this.settings.allowLowerCase ? newP4
+                                                         : newP4.charAt(0).toUpperCase() + newP4.slice(1);
+                }
+            } else {
+                // Plain intra-wiki link
+                newP4 = this.settings.allowLowerCase ? p4
+                                                     : p4.charAt(0).toUpperCase() + p4.slice(1);
+            }
+
+            // Process newP4
+            newP4 = newP4.replace(" ", "_");
+            newP6 = this._pipeLink(newP6, p5, p6);
+            return `<a href='${this.getBaseURL(server) + "/" + newP4}'>${newP6 ? newP6 : p4}</a>`;
         }).bind(this);
         
         let text = wikimarkup;
--- test/test-parser.js
+++ test/test-parser.js
@@ -64,7 +64,7 @@ b==B==`;
     });
 
     describe("_parseLinks", function() {
-        it("Should parse existent links without colons", function() {
+        it("Should parse simple intrawiki links", function() {
             const wikitext = [
                                 "[[abc]]",
                                 "[[pip_ing]]",
@@ -72,7 +72,7 @@ b==B==`;
                                 "[[aaa()bbb]]",
                                 "[[a b]]",
                                 "[[abc|def]]",
-                                "[[a b|]]"
+                "[[a b|]]"
                              ];
             const parseFn  = parser._parseLinks.bind(parser);
             const result   = wikitext.map(parseFn);
@@ -86,6 +86,159 @@ b==B==`;
                                 `<a href='${url}/Abc'>def</a>`,
                                 `<a href='${url}/A_b'>a b</a>`
                              ];
+            result.forEach((item, idx) => {
+                expect(item).to.equal(answers[idx]);
+            });
+        });
+
+        it("Should parse interwiki links", function() {
+            // Set up parser
+            parser.settings.server = {
+                protocol: "http",
+                subdomain: "en",
+                domain: "wikipedia",
+                tld: "org",
+                path: "wiki"
+            };
+            
+            parser.settings.interwiki.push({
+                server: { protocol: "http",
+                    subdomain: "www",
+                    domain: "wikisource",
+                    tld: "org",
+                    path: "wiki"
+                },
+                indicators: ["s", "wikisource"]
+            }, {
+                server: { protocol: "http",
+                    subdomain: "www",
+                    domain: "wikiquote",
+                    tld: "org",
+                    path: "wiki"
+            },
+                indicators: ["q", "wikiquote"]
+            }, {
+                server: { protocol: "http",
+                    subdomain: "www",
+                    domain: "wikipedia",
+                    tld: "org",
+                    path: "wiki"
+                },
+                indicators: ["w", "wikipedia"]
+            });
+
+            // Run test
+            const wikitext = [
+                "[[s:abc]]",
+                "[[s:Zyz]]",
+                "[[wikisource:es:def]]",
+                "[[q:bde]]",
+                "[[wikiquote:elf]]",
+                "[[s:zzz|bbb]]",
+                "[[s:zzz|]]",
+                "[[s:es:aaa|]]"
+            ];
+            const parseFn  = parser._parseLinks.bind(parser);
+            const result   = wikitext.map(parseFn);
+            const answers  = [
+                "<a href='http://en.wikisource.org/wiki/abc'>s:abc</a>",
+                "<a href='http://en.wikisource.org/wiki/Zyz'>s:Zyz</a>",
+                "<a href='http://en.wikisource.org/wiki/es:def'>wikisource:es:def</a>",
+                "<a href='http://en.wikiquote.org/wiki/bde'>q:bde</a>",
+                "<a href='http://en.wikiquote.org/wiki/elf'>wikiquote:elf</a>",
+                "<a href='http://en.wikisource.org/wiki/zzz'>bbb</a>",
+                "<a href='http://en.wikisource.org/wiki/zzz'>zzz</a>",
+                "<a href='http://en.wikisource.org/wiki/es:aaa'>es:aaa</a>"
+            ];
+            result.forEach((item, idx) => {
+                expect(item).to.equal(answers[idx]);
+            });
+        });
+
+        it("Should parse intersubdomain links", function() {
+            // Set up parser
+            parser.settings.server = {
+                protocol: "http",
+                subdomain: "en",
+                domain: "wikipedia",
+                tld: "org",
+                path: "wiki"
+            };
+            
+            parser.settings.interwiki.push({
+                server: { protocol: "http",
+                    subdomain: "www",
+                    domain: "wikipedia",
+                    tld: "org",
+                    path: "wiki"
+                },
+                indicators: ["w", "wikipedia"],
+                subdomains: ["en", "fr"]
+            });
+
+            // Run test
+            const wikitext = [
+                "[[fr:abc]]",
+                "[[:fr:def]]",
+                "[[zzz:Zyz]]",
+                "[[:fr:abc|]]",
+                "[[:fr:abc|def]]"
+
+            ];
+            const parseFn  = parser._parseLinks.bind(parser);
+            const result   = wikitext.map(parseFn);
+            const answers  = [
+                "",
+                "<a href='http://fr.wikipedia.org/wiki/def'>fr:def</a>",
+                "<a href='http://en.wikipedia.org/wiki/Zzz:Zyz'>zzz:Zyz</a>",
+                "<a href='http://fr.wikipedia.org/wiki/abc'>abc</a>",
+                "<a href='http://fr.wikipedia.org/wiki/abc'>def</a>"
+            ];
+            result.forEach((item, idx) => {
+                expect(item).to.equal(answers[idx]);
+            });
+        });
+
+        it("Should parse inter-namespace links", function() {
+            // Set up parser
+            parser.settings.server = {
+                protocol: "http",
+                subdomain: "en",
+                domain: "wikipedia",
+                tld: "org",
+                path: "wiki"
+            };
+            
+            parser.settings.interwiki.push({
+                server: { protocol: "http",
+                    subdomain: "www",
+                    domain: "wikipedia",
+                    tld: "org",
+                    path: "wiki"
+                },
+                indicators: ["w", "wikipedia"],
+                subdomains: ["en", "fr"]
+            });
+
+            // Run test
+            const wikitext = [
+                "[[Template:Test]]",
+                "[[Wikipedia:Test]]",
+                "[[Talk:Test]]",
+                "[[Category:Test]]",
+                "[[:Category:Test]]",
+                "[[Template:Test|]]"
+            ];
+            const parseFn  = parser._parseLinks.bind(parser);
+            const result   = wikitext.map(parseFn);
+            const answers  = [
+                "<a href='http://en.wikipedia.org/wiki/Template:Test'>Template:Test</a>",
+                "<a href='http://en.wikipedia.org/wiki/Wikipedia:Test'>Wikipedia:Test</a>",
+                "<a href='http://en.wikipedia.org/wiki/Talk:Test'>Talk:Test</a>",
+                "",
+                "<a href='http://en.wikipedia.org/wiki/Category:Test'>Category:Test</a>",
+                "<a href='http://en.wikipedia.org/wiki/Template:Test'>Test</a>"
+            ];
             result.forEach((item, idx) => {
                 expect(item).to.equal(answers[idx]);
             });
