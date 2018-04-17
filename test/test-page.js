111465c1950d3aa924bbba57d2f9c5be0d0f601a
Ryan Dabler <rpdabler@gmail.com>
2018-04-16 21:19:52 -0600

    implement tests for a few Page methods

--- test/test-page.js
+++ test/test-page.js
@@ -33,4 +33,34 @@ describe("Page", function() {
             expect(page.unnamedExternalLinks).to.deep.equal(links);
         });
     });
+
+    describe("setPlaceholder", function() {
+        it("Should generate a placeholder for supplied text and return it", function() {
+            const text = "abc";
+            const placeholder = page.setPlaceholder(text);
+            
+            expect(page.placeholders.size).to.equal(1);
+            expect(page.placeholders.get(placeholder)).to.equal(text);
+        });
+    });
+
+    describe("replacePlaceholders", function() {
+        let text1, text2;
+        let ph1, ph2;
+        let fillerText;
+
+        beforeEach(function() {
+            fillerText = `\n\nTest\n\n`;
+            [ text1, text2 ] = [ "abc", "def" ];
+            [ ph1, ph2 ] = [ page.setPlaceholder(text1), page.setPlaceholder(text2) ];
+            page.html = `${ph1}${fillerText}${ph2}`;
+        });
+
+        it("Should replace all placeholders with original text", function() {
+            page.replacePlaceholders();
+            
+            expect(page.html).to.equal(`${text1}${fillerText}${text2}`);
+            expect(page.placeholders.size).to.equal(0);
+        });
+    });
 });
